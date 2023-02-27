2a0d136bb23aeccb634f81199586065de2716075
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-27 21:25:49 +0200

    Install Notify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+Notify.failure('Oops, there is no country with that name');
+Notify.info('Too many matches found. Please enter a more specific name.');
